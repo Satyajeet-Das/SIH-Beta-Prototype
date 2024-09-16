31940ed5dccdfeeee33c4aad5ad8dc6ad3aae8fc
Soham <umbaresoham18@gmail.com>
2024-09-16 22:15:09 +0530

    fix: added colors to the skill-tags and the difficulty levels

--- frontend/src/components/Skilltags.tsx
+++ frontend/src/components/Skilltags.tsx
@@ -12,20 +12,21 @@ interface Props{
 const SkillTags = ({text, difficulty, type}: Props) => {
 
     const difficultyClass = difficulty === 1 
-    ? "text-white" 
+    ? "text-[#4BB5FF]" //#5fd776
     : difficulty === 2 
-    ? "text-blue-300" 
-    : "text-orange-300";
+    ? "text-[#FFC107]" //#e66a00
+    : "text-[#00695C]";  //#b52b32
 
     return(
         <>
             {type ? (
-                <span className="mr-4 mb-2 px-4 py-1 font-bold text-white bg-red-400 mt-2 rounded-full">
+                <span className="mr-4 mb-2 px-4 py-1 font-bold text-[#B7300C] bg-[#FFF1EE] border-2 border-[#FFD2C2] mt-2 rounded-xl">
                     <FontAwesomeIcon className={`mr-1 text-sm ${difficultyClass}`} icon={faCircle} />
                     {text}
                 </span>
             ) : (
-                <span className="mr-4 mb-2 px-4 py-1 font-bold text-white bg-[#00acee] mt-2 rounded-full">
+                <span className="mr-4 mb-2 px-4 py-1  text-[#5228CD] bg-[#ebe6f9] mt-2 rounded-xl">
+                    <FontAwesomeIcon className={`mr-1 text-sm ${difficultyClass}`} icon={faCircle} />
                     {text}
                 </span>
             )}
