a792149feedb1b3d9a2094e6c7576d175239e4a7
Soham <umbaresoham18@gmail.com>
2024-09-17 01:10:49 +0530

    fix: added new colors to the skill-tags

--- frontend/src/components/Skilltags.tsx
+++ frontend/src/components/Skilltags.tsx
@@ -20,13 +20,13 @@ const SkillTags = ({text, difficulty, type}: Props) => {
     return(
         <>
             {type ? (
-                <span className="mr-4 mb-2 px-4 py-1 font-bold text-[#B7300C] bg-[#FFF1EE] border-2 border-[#FFD2C2] mt-2 rounded-xl">
+                <span className="mr-4 mb-2 px-4 py-1 font-bold text-[#B7300C] bg-gradient-to-r from-red-200 via-[#FFF1EE] to-purple-100 border-2 border-[#e51c19] mt-2 rounded-2xl">
                     <FontAwesomeIcon className={`mr-1 text-sm ${difficultyClass}`} icon={faCircle} />
                     {text}
                 </span>
             ) : (
                 // <span className="mr-4 mb-2 px-4 py-1 font-bold  text-[#9679ec] bg-[#ebe6f9] mt-2 rounded-xl border-2 border-[#D1B2FD]">
-                <span className="mr-4 mb-2 px-4 py-1 font-semibold  text-white bg-gradient-to-l from-purple-300 via-blue-600 to-blue-500   mt-2 rounded-xl " >
+                <span className="mr-4 mb-2 px-4 py-1 font-semibold  text-violet-500 bg-gradient-to-l from-purple-100 via-[#ebe6f9] to-blue-200   mt-2 rounded-xl " >
                     <FontAwesomeIcon className={`mr-1 text-sm ${difficultyClass}`} icon={faCircle} />
                     {text}
                 </span>
