ba28747fc1ba47ebf82c872ee6ce305968b21244
Shivanshu Gupta <shivanshukgupta@gmail.com>
2024-09-18 13:10:21 +0530

    feat: added a resume bttn

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,8 +1,8 @@
+import { faBars, faTimes } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { useState } from "react";
 import { Link } from "react-router-dom";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faBars, faTimes } from "@fortawesome/free-solid-svg-icons";
-import logo from "./logo.png"
+import logo from "./logo.png";
 
 
 const Header = () => {
@@ -13,22 +13,28 @@ const Header = () => {
       <div className="px-9 mx-auto flex justify-between items-center">
         {/* Sidebar Toggle Button */}
         <div className="flex items-center space-x-4">
-            <FontAwesomeIcon
+          <FontAwesomeIcon
             className="cursor-pointer text-white md:hidden"
             icon={isSidebarOpen ? faTimes : faBars}
             onClick={() => setIsSidebarOpen(!isSidebarOpen)} // Toggle sidebar visibility
-            />
+          />
 
-            {/* Website Name */}
-           
-            <Link to="/" className="flex items-center text-sans text-xl md:text-2xl text-white font-bold tracking-tight">
-              <img src={logo} alt="Dhrti" className="h-9 w-auto " />
-            </Link>
-            
+          {/* Website Name */}
+
+          <Link to="/" className="flex items-center text-sans text-xl md:text-2xl text-white font-bold tracking-tight">
+            <img src={logo} alt="Dhrti" className="h-9 w-auto " />
+          </Link>
+
         </div>
 
         {/* Links for medium screens and up */}
         <div className="hidden md:flex space-x-1 text-sm md:text-base">
+          <button className="text-white rounded-full py-1 px-4 font-bold bg-gradient-to-br from-[#00fcd3] from-10% via-[#2f74db] via-45% to-[#b63cb3] to-90% hover:from-[#00e6bf] hover:via-[#2968c5] hover:to-[#a834a1] transition duration-300 flex items-center space-x-2">
+            <img src="src/assets/ai.webp" alt="Dhrti" className="h-9 w-auto filter invert brightness-0" />
+            Generate Resume
+          </button>
+
+
           <Link
             to="/sign-in"
             className="flex items-center text-white py-1 px-5 font-bold rounded-2xl hover:text-green-700 hover:bg-white"
